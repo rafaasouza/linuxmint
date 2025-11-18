34521befbe8780cb1515bd679fe8e10bfedca7e0
Rafael Souza <rafael.almeidasouza95@gmail.com>
2025-11-17 22:15:50 -0300

    Atualização

---
+++ comandos/comandos.sh
@@ -0,0 +1,38 @@
+1  curl -fsS https://dl.brave.com/install.sh | sh
+    2  tor
+    3  torbrowser-launcher
+    4  sudo apt install torbrowser-launcher
+    5  sudo apt-get install snapd snapd-xdg-open
+    6  ``` [12]
+    7  sudo snap install notepad-plus-plus
+    8  ``` [7, 12]
+    9  sudo apt-get install notepadqq
+   10  sudo apt install git vim notepadqq
+   11  sudo apt update
+   12  sudo apt install git vim notepadqq 
+   13  sudo init 0
+   14  sudo apt update
+   15  sudo
+   16  sudo -i
+   17  sudo sources apt
+   18  sudo apt
+   19  sudo apt 
+   20  sudo apt upda
+   21  sudo apt update
+   22  sudo apt upgrade
+   23  sudo apt dist-upgrade 
+   24  sudo apt full-upgrade 
+   25  sudo apt autoclean 
+   26  sudo apt clean
+   27  sudo apt install git vim python3 pip
+   28  sudo apt install git vim pip python
+   29  git clone https://github.com/rafaasouza/linuxmint
+   30  git config --global user.name "Seu Usuario"
+   31  git config --global user.email seu email
+   32  vim
+   33  git config --global core.editor vim
+   34  git confi --global merge.tool vimdiff
+   35  git config --global merge.tool vimdiff
+   36  git config --global color.ui true
+   37  git config --list 
+   38  history
